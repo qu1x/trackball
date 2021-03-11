6199a6571cab0da6815dd6c0d04033ec61ba9e96
Rouven Spreckels <rs@qu1x.dev>
2021-03-11 21:28:01 +0100

    Fix version.

--- c11/src/trackball.h
+++ c11/src/trackball.h
@@ -16,7 +16,7 @@
 /// [S2CID]: https://en.wikipedia.org/wiki/S2CID_(identifier)
 /// [44199608]: https://api.semanticscholar.org/CorpusID:44199608
 ///
-/// \version v0.1.0
+/// \version v0.1.1
 /// \author Rouven Spreckels <rs@qu1x.dev>
 /// \copyright BSD-3-Clause
 ///
